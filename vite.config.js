4bd6e8ff60255c6ba42847da65e7f6f86572cbba
Rafael Teyral <r.teyral@tricentis.com>
2024-03-25 20:30:29 +0100

    make it work in dev

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import { createVuePlugin as vue } from "vite-plugin-vue2";
 const path = require("path");
+import { VuetifyResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    Components({
+      resolvers: [
+        // Vuetify
+        VuetifyResolver(),
+      ],
+    }),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
